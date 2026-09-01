a0317c366aa1d97595a4542b769759d374d6f40b
Bortoliseguros <pf.federal@gmail.com>
2026-09-01 13:46:59 -0300

    Update page title for dashboard

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ from openpyxl.utils import get_column_letter
 
 st.set_page_config(page_title="Dashboard e Gestão de Riscos", layout="wide")
 
-st.title("📊 Painel de Vendas e Gestão de Riscos")
+
 # Mascaramento de CPF
 def mascarar_cpf(val):
     if pd.isna(val):
